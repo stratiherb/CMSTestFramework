1467cfdb8a8c3ef007e7dd7404a270a429d847e4
Strat Aguilar <strat.aguilar@iherb.com>
2016-10-28 17:04:07 -0700

    carthage ready

---
+++ Carthage/Build/iOS/CMSTestFramework.framework/Headers/CMSTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  CMSTestFramework.h
+//  CMSTestFramework
+//
+//  Created by SAguilar on 10/27/16.
+//  Copyright © 2016 SAguilar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMSTestFramework.
+FOUNDATION_EXPORT double CMSTestFrameworkVersionNumber;
+
+//! Project version string for CMSTestFramework.
+FOUNDATION_EXPORT const unsigned char CMSTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMSTestFramework/PublicHeader.h>
+
+
